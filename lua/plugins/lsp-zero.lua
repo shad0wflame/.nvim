a6a4c329cd4c1088d32e5cbadde8cf7818d89974
Uriel Huertas Garriga <shad0wflame@grayfire-540.local>
2024-12-04 13:46:28 +0100

    feat: added missing lsp autoconfiguration

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -98,6 +98,11 @@ return {
           'vtsls'
         },
         handlers = {
+          function(server_name)
+            require('lspconfig')[server_name].setup({
+              capabilities = capabilities,
+            })
+          end,
           lua_ls = function()
             local lua_opts = lsp_zero.nvim_lua_ls()
             require('lspconfig').lua_ls.setup(lua_opts)
